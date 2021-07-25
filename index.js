223248f0e12bef3cd4cc714bd5399fe0cb302354
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-07-24 23:05:27 -0400

    completed additional classes with import/export

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Employee } = require('./lib/employee');
 const { Manager } = require('./lib/manager');
+const { Engineer } = require('./lib/engineer');
+const { Intern } = require('./lib/intern');
 
-const bob = new Manager('bob123@fakemail.com', 101);
+const bob = new Manager('Bob', 'bob123@fakemail.com', 101);
 
-console.log({bob});
+console.log({bob}, bob.getRole());
