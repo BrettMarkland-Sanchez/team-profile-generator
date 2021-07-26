aafa442a4828fa8328a6cbdbad7c85b1365fdd09
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-07-25 21:02:51 -0400

    degugged problem with too many listeners, removed validation

--- index.js
+++ index.js
@@ -3,84 +3,144 @@ const { Manager } = require('./lib/manager');
 const { Engineer } = require('./lib/engineer');
 const { Intern } = require('./lib/intern');
 const inquirer = require('inquirer');
-const validator = require('validator');
 
 // Staff array for storing Employee objects
 const staff = [];
 
 // Start function for running the program
 function start() {
+    // Starts with manager() then chains to query()
     manager();
-    query();
 }
 
 // Inquirer function for gathering new Manager data
 function manager() {
+    console.log('Please provide information for your team manager.\n');
     inquirer.prompt([ // Manager data input section
         {   
             type: 'input',
-            message: `What is the Manager's name?`,
+            message: `What is the manager's name?`,
             name: 'name'
         },
         {
-            type: 'input',
-            message: `What is the Manager's ID number?`,
+            type: 'number',
+            message: `What is the manager's ID number?`,
             name: 'id'
         },
         {
             type: 'input',
-            message: `What is the Manager's e-mail?`,
+            message: `What is the manager's e-mail?`,
             name: 'email'
         },
         {
             type: 'input',
-            message: `What is the Manager's office number?`,
+            message: `What is the manager's office number?`,
             name: 'office'
         }
-    ]).then((input) => { // User input validation
-        if(typeof input.name !== "string"){
-            console.error("The given name was not in string format. Please try again.");
-            manager();
-        }
-        if(typeof input.id !== "number"){
-            console.error("The given employee ID was not in number format. Please try again.");
-            manager();
-        }
-        if(!validator.isEmail(input.email)){
-            console.error("The given email was not in a valid format. Please try again.");
-            manager();
-        }
-        if(!(typeof input.office == "string" || typeof input.office == "number")){
-            console.error("The given office number was not in a valid format. Please try again.");
-            manager();
-        }
-        return input;
-    }).then((data) => { // New Manager creation
+    ]).then((data) => { // New Manager creation
         let employee = new Manager(data.name, data.id, data.email, data.office);
         // Add new Manager to the staff array
         staff.push(employee);
-    })
+    }).then(() => query()); // Chain more options from query()
 }
 
-// Inquirer function for gathering additional Employee data
+// Inquirer function for handling additional Employee data
 function query() {
     inquirer.prompt([ // Additional Employee data input section
         {   
             type: 'list',
             message: `What do you want to do next?`,
-            name: 'query'
+            name: 'query',
+            choices: [
+                'Add an Engineer to your team roster',
+                'Add an Intern to your team roster',
+                'Exit program and generate team profile'
+            ]
         }
-    ])
+    ]).then((input) => { // Switch handles function calling
+        switch(input.query) {
+            case 'Add an Engineer to your team roster': {
+                engineer();
+            } break;
+            case 'Add an Intern to your team roster': {
+                intern();
+            } break;
+            case 'Exit program and generate team profile': {
+                render();
+            } break;
+        };
+    });
+}
+
+// Inquirer function for gathering new Engineer data
+function engineer() {
+    console.log('Please provide information for your new engineer.');
+    inquirer.prompt([ // Engineer data input section
+        {   
+            type: 'input',
+            message: `What is the engineer's name?`,
+            name: 'name'
+        },
+        {
+            type: 'number',
+            message: `What is the engineer's ID number?`,
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `What is the engineer's e-mail?`,
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: `What is the engineer's GitHub username?`,
+            name: 'github'
+        }
+    ]).then((data) => { // New Engineer creation
+        let employee = new Engineer(data.name, data.id, data.email, data.github);
+        // Add new Engineer to the staff array
+        staff.push(employee);
+    }).then(() => query()); // Chain more options from query()
+}
+
+// Inquirer function for gathering new Intern data
+function intern() {
+    console.log('Please provide information for your new intern.');
+    inquirer.prompt([ // Intern data input section
+        {   
+            type: 'input',
+            message: `What is the intern's name?`,
+            name: 'name'
+        },
+        {
+            type: 'number',
+            message: `What is the intern's ID number?`,
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `What is the intern's e-mail?`,
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: `What school does the intern attend?`,
+            name: 'school'
+        }
+    ]).then((data) => { // New Intern creation
+        let employee = new Intern(data.name, data.id, data.email, data.school);
+        // Add new Intern to the staff array
+        staff.push(employee);
+    }).then(() => query()); // Chain more options from query()
 }
 
 // Uses code from render.js to create cards from staff array values
 function render(arr) {
-    // Use a for-of loop here to generate HTML 
+    // Use a for-of loop here to generate HTML
+
+    // Print staff array for testing
+    console.log(staff);
 }
 
 // Calls function start
 start();
-
-
-
-
